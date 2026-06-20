0ac2f5bfdc0ec60bff42490c46c1c56abfd9c59b
Mostafizur01 <mostafizurrahman43@gamil.com>
2026-06-20 13:32:35 +0600

    backend fix4

--- backend/function/fakeData.js
+++ backend/function/fakeData.js
@@ -13,12 +13,16 @@ function genaretMusicData (userSeed, page, lang) {
     const fakeData = []
     const count = faker.number.int({ min: 50, max: 100 })
     for (let i = 1; i <= count; i++) {
+
         fakeData.push({
             id: i + 1,
             title: faker.music.songName(),
             artist: faker.person.fullName(),
             album: faker.music.album(),
-            genre: faker.music.genre()
+            genre: faker.music.genre(),
+            like: faker.number.int({ min: 0, max: 2000 }),
+            text: faker.lorem.paragraphs(2),
+            duration: `${Math.floor(Math.random() * 4)} : ${Math.floor(Math.random() * 60)}`
         })
     }
     return fakeData
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import heroImg from './assets/hero.png'
 
 function App() {
   const [data, setData] = useState(null)
-  const [loding, setLoding] = useState(false)
+  const [loding, setLoding] = useState(true)
   useEffect(() => {
-    fetch('https://task5-backend-y7lw.onrender.com')
+    fetch('https://task5-backend-y7lw.onrender.com/api/data?seed=1&page=1')
       .then(res => res.json())
       .then(data => {
         setData(data)
@@ -19,10 +19,37 @@ function App() {
         setLoding(false)
       })
   }, [])
+  if (loding) return <div>loding.....</div>
+  if (!data) return <div>Data not found</div>
   return (
     <>
-      
-      
+      <div className="music-container">
+    <table className="music-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Song</th>
+          <th>Artist</th>
+          <th>Album</th>
+          <th>Genre</th>
+          <th>Likes</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.artist}</td>
+            <td>{item.album}</td>
+            <td>{item.genre}</td>
+            <td>{item.like} ❤️</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
     </>
   )
 }
