a91a49d8262c974be069f56b33139070ac9fe292
Mostafizur01 <mostafizurrahman43@gamil.com>
2026-06-20 12:14:11 +0600

    fix backend 3

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import fakeData from './function/fakeData.js'
 import createMidiWriter from './function/midiWriter.js'
 
 const app = express()
-app.use(cors({ origin: 'http://localhost:5173/'}))
+app.use(cors({ origin: 'http://localhost:5173'}))
 app.use(express.json())
 const port = process.env.PORT || 3000
 
